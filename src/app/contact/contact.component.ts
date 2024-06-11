aef650b550b96aff3e8af3c326178e639e8482f2
Tanushree-jain <tanushreegangwal19@gmail.com>
2024-06-11 12:57:17 +0530

    google form seeting update

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,37 +1,14 @@
-import { HttpClient } from '@angular/common/http';
-import { Component} from '@angular/core';
-import { FormGroup,FormBuilder,Validators } from '@angular/forms';
+import { Component, ElementRef} from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
-  contactForm: FormGroup;
-
-  private googleScriptURL = '';
-
-  constructor(private fb: FormBuilder, private http: HttpClient) {
-    this.contactForm = this.fb.group({
-      name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      message: ['', Validators.required],
-    });
-  }
-
-  onSubmit() {
-    if (this.contactForm.valid) {
-      const formData = this.contactForm.value;
-
-      this.http.post(this.googleScriptURL, formData).subscribe(
-        response => {
-          alert('Your message has been sent successfully!');
-          this.contactForm.reset();
-        },
-        error => {
-          alert('There was an error sending your message. Please try again later.');
-        }
-      );
-    }
+  constructor(private el: ElementRef) {}
+  
+  onIframeLoad() {
+    const iframe = this.el.nativeElement.querySelector('iframe');
+    iframe.contentWindow.postMessage({ type: 'GET_FORM_DATA' }, '*');
   }
 }
