64a4d31669bb0a79030c135aa8b71c84568fc06a
Tanushree-jain <tanushreegangwal19@gmail.com>
2025-01-08 20:06:12 +0530

    update changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'portfolio';
+  synth: any;
+  voices: any;
+  constructor(){
+    this.synth = window.speechSynthesis;
+    this.voices = this.synth.getVoices();
+  }
+ngOnInit(): void {
+  this.speak("Hey! My name is Tanushree Gangwal. I am a Full Stack Developer");
+  
+}
 
 
+speak(string: any) {
+  let u = new SpeechSynthesisUtterance(string);
+  u.text = string;
+  u.lang = "en-US";
+  u.volume = 1; //0-1 interval
+  u.rate = 1;
+  u.pitch = 2; //0-2 interval
+  this.synth.speak(u);
+}
 }
